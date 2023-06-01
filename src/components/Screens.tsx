5516f8018bc30a41d0d042b3d4741c765bebf4b5
Nick Cheatwood <nick.cheatwood@gmail.com>
2023-05-31 20:41:13 -0400

    💅 styles: add condition color

--- src/components/Screens.tsx
+++ src/components/Screens.tsx
@@ -1,15 +1,33 @@
+import '~/styles/Screens.css';
+
 export function Xs() {
-  return <h1>Xs</h1>;
+  return (
+    <div className='screen xs-screen'>
+      <h1>Xs</h1>
+    </div>
+  );
 }
 
 export function Sm() {
-  return <h1>Sm</h1>;
+  return (
+    <div className='screen sm-screen'>
+      <h1>Sm</h1>
+    </div>
+  );
 }
 
 export function Md() {
-  return <h1>Md</h1>;
+  return (
+    <div className='screen md-screen'>
+      <h1>Md</h1>
+    </div>
+  );
 }
 
 export function Lg() {
-  return <h1>Lg</h1>;
+  return (
+    <div className='screen lg-screen'>
+      <h1>Lg</h1>
+    </div>
+  );
 }
